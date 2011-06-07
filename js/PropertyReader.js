a878b297ae8f11355e4b84ec6befed21d220d773
Maximiliano Fierro <elmasse@gmail.com>
2011-06-07 11:58:30 -0300

    Adding proper comments lines and logical and natural lines support for Properties file.

--- js/PropertyReader.js
+++ js/PropertyReader.js
@@ -3,12 +3,9 @@ Ext.ns('Ext.i18n');
  * 
  * PropertyReader is used to read data from a .properties files
  * The .properties file might be in the traditional format which means that comments are accepted.
- * key and value must be separated by the specified propertySeparator. By default a space is used.
- * value must be in between " and not '
  * 
  * @class Ext.i18n.PropertyReader
  * @constructor
- * @param config.propertySeparator {String}. The String that is used as separator between key and value. Default " ". Optional
  * 
  * @author Maximiliano Fierro (elmasse)
  */
@@ -35,10 +32,6 @@ Ext.i18n.PropertyReader = function(config){
 }
 
 Ext.extend(Ext.i18n.PropertyReader, Ext.data.Reader,{
-	/**
-	 * @cfg propertySeparator: {String}. Default: " ". Optional
-	 */
-	propertySeparator: " ",	
 
 	read: function(response){
 		var propertyFile = response.responseText;
@@ -49,53 +42,42 @@ Ext.extend(Ext.i18n.PropertyReader, Ext.data.Reader,{
 	},
 	
 	readRecords: function(propertyFile){
-		var totalRecords = 0, success = true;
-		var Record = this.recordType;
-		var records = [];
+		var Record = this.recordType,
+			records = [], record, kv,
+			f = this.readLines(propertyFile),
+			l = f.length;
 		
-		var f = this.readLines(propertyFile);
-		
-		for(var i = 0; i < f.length; i++){
-			var key, value, kLim;
-			kLim = f[i].indexOf(this.propertySeparator);
-			key = String(f[i].substring(0, kLim));
-			value = f[i].substring(kLim+1).trim().replace(/\"/g, '');
-			
-			var record = Ext.ModelMgr.create({
-			    value : value,
-			    key  : key
-			}, 'PropertyModel');
-			//bad Model implementation
-			record.id = key;	
-			records[i] = record;
+		for(var i = 0; i < l; i++){
+			var kl = f[i].search(/[\s:=]/);
+				record = Ext.ModelMgr.create({
+				    value : this.clearValueExtraChars(f[i].substring(kl+1)),
+				    key  :  this.clearKeyExtraChars(f[i].substring(0, kl))
+				}, 'PropertyModel');
+
+				records[i] = record;
 		}
 		
 		return new Ext.data.ResultSet({
             total  : records.length,
             count  : records.length,
             records: records,
-            success: success
+            success: true
         });
-	
 	},
 	
-	createAccessor: function(){
-		
+	createAccessor: function(){},
+	
+	clearKeyExtraChars: function(s){
+		return (s ? s.replace(/[:=]/gi, "") : "");
+	},
+	
+	clearValueExtraChars: function(s){
+		return (s ? s.replace(/\\\s*\n/gi, "") : "");
 	},
 	
 	//private
 	readLines: function(file){
-		var aux = String(file).split('\n');
-		var lines = new Array();
-		
-		for(var i = 0; i < aux.length; i++){
-			if(aux[i].indexOf("#") < 0 || (aux[i].indexOf("#") > aux[i].indexOf("\""))){
-				var line = aux[i];//Ext.util.Format.trim(aux[i]);
-				if(line)
-					lines.push(line);
-			}	
-		}
-		return lines;
+		return (file ? file.match(/.*(.*\\\s*\n)+.*|^((?!^\s*[#!]).).*$/gim) : []);
 	}
 
 });
